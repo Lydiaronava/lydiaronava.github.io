7e5162635d3995260c1090f7fe1f5e94a915116c
Lydiaronava <lydiaronava@gmail.com>
2026-09-02 22:11:25 +0200

    indy

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -4,6 +4,16 @@ function Body() {
     return (
         <div className="content">
             <p>Website under construction... come back soon!</p>
+            <div>
+                <p>Meanwhile, please enjoy this picture of a cute dog.</p>
+                <img 
+                    class="indy" 
+                    src="src/assets/indy.jpg" 
+                    alt="A picture of a small white dog, a bichon maltais, sitting on a colorful folding chair." 
+                    width="50%"
+                    height="50%"
+                />
+            </div>
         </div>
     )
 }
