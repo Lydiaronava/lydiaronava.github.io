242ba1bce242bfa1a52afb5212429e83d5137caa
Lydiaronava <lydiaronava@gmail.com>
2026-09-02 19:48:03 +0200

    init vite site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lydia Rodriguez de la Nava</h1>
+      <p>Website under construction... come back soon!</p>
+    </>
+  )
+}
+
+export default App
