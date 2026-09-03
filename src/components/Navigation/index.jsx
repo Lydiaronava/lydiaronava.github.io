f0940f86b8c0b9e84bef5c4dcb568c6aacc6d9b5
Lydiaronava <lydiaronava@gmail.com>
2026-09-03 13:35:33 +0200

    update layout

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -4,16 +4,19 @@ function  Navigation() {
     return (
         <header className="header">
             <div className="header-content">
-                <h1 className="header-logo">Lydia Rodriguez de la Nava</h1>
-                {/* <h2 className="subtitle">Reseacher in computer science applied to music</h2> */}
-                <nav className="nav">
-                    <a href="#home" className="nav-link">Home</a>
-                    <a href="#publications" className="nav-link">Publications</a>
-                    <a href="#teaching" className="nav-link">Teaching</a>
-                    <a href="#CV" className="nav-link">CV</a>
-                    <a href="#phd-thesis" className="nav-link">PhD thesis</a>
-                    <a href="#contact" className="nav-link">Contact</a>
+                <div className="page-title">
+                    <h1 className="header-name">Lydia Rodriguez de la Nava</h1>
+                    <h2 className="subtitle">Reseacher in computer science applied to music</h2>
+                </div>
+                <nav className="nav-buttons">
+                        <a href="#home" className="nav-link">Home</a>
+                        <a href="#publications" className="nav-link">Publications</a>
+                        <a href="#teaching" className="nav-link">Teaching</a>
+                        <a href="#CV" className="nav-link">CV</a>
+                        <a href="#phd-thesis" className="nav-link">PhD thesis</a>
+                        <a href="#contact" className="nav-link">Contact</a>
                 </nav>
+
             </div>
         </header>
     )   
